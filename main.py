b3749d1f19a7fc665f571a42faf4c7f470919b1c
Miriam <m.aumatell33@gmail.com>
2023-01-19 11:15:02 +0100

    unify code in aux methods

--- main.py
+++ main.py
@@ -7,22 +7,30 @@ import pickle
 
 app = FastAPI()
 
+model_file = "model.pickle"
+model = pickle.load(open(model_file, "rb"))
 
 @app.get("/")
-def base(q_sr_id: str, m_sr_id: str): 
+def base(q_sr_id: str, m_sr_id: str):
     con = sqlite3.connect("db.db")
     q = pd.read_sql_query(f"SELECT * from soundrecording WHERE sr_id = '{q_sr_id}'", con).set_index("id")
     m = pd.read_sql_query(f"SELECT * from soundrecording WHERE sr_id = '{m_sr_id}'", con).set_index("id")
-    title_sim = fuzz.ratio(q.title.to_list()[0], m.title.to_list()[0])
-    artists_sim = fuzz.ratio(q.artists.to_list()[0], m.artists.to_list()[0])
-    contributors_sim = fuzz.ratio(q.contributors.to_list()[0], m.contributors.to_list()[0])
-    isrcs_coincidence = fuzz.ratio(q.isrcs.to_list()[0], m.isrcs.to_list()[0])
+    
+    def compute_word_similarity(ds1, ds2, column_name):
+        return fuzz.ratio(ds1[column_name].to_list()[0], ds2.title.to_list()[0])
+    def compute_conicidence(ds1, ds2, column_name):
+        if ds1[column_name].to_list()[0] == ds2[column_name].to_list()[0]:
+            return 1
+        return 0
+        
+    title_sim = compute_word_similarity(q, m, "title")
+    artists_sim = compute_word_similarity(q, m, "artists")
+    contributors_sim = compute_word_similarity(q, m, "contributors")
+    isrcs_coincidence = compute_conicidence(q, m, "isrcs")
     x_predict = [title_sim, artists_sim, contributors_sim, isrcs_coincidence]
     
-    filename = "model.pickle"
-    loaded_model = pickle.load(open(filename, "rb"))
     X_sample = np.array(x_predict).reshape(1,-1).tolist()
-    pred = loaded_model.predict(X_sample).tolist()[0]
+    pred = model.predict(X_sample).tolist()[0]
     result = {}
     result["class"] = "valid" if pred==1 else "invalid"
     return result
